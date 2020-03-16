286b7e39ee97593fa6d9d662128701c922d5b99b
bhargavak <19756773+kbhagi@users.noreply.github.com>
2020-03-16 08:47:12 +0530

    Add files via upload

---
+++ RemoteMethodExecution/Adder.java
@@ -0,0 +1,17 @@
+
+import java.rmi.*;
+import java.rmi.server.*;
+
+
+public class Adder  extends UnicastRemoteObject  implements AddServerInterface
+{
+  
+	public Adder() throws RemoteException
+     {
+        super();
+    }
+    public int sum(int a,int b)
+    {
+        return a+b;
+    }
+}
